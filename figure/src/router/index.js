07637f220e85650d593e3b00acc2e17cc22a2145
wypym <329259203@qq.com>
2018-12-20 19:07:12 +0800

    框架

---
+++ figure/src/router/index.js
@@ -0,0 +1,11 @@
+import Vue from 'vue'
+import Router from 'vue-router'
+import App from '../App.vue'
+
+Vue.use(Router)
+
+export default new Router({
+  routes: [
+    {path: '/',component: App}
+  ]
+})
---
+++ figure/src/store/index.js
@@ -0,0 +1,20 @@
+import Vue from 'vue';
+import Vuex from 'vuex';
+Vue.use(Vuex)
+
+const store = new Vuex.store({
+    state:{
+
+    },
+    mutations:{
+
+    },
+    getters:{
+
+    },
+    actions:{
+
+    }
+})
+
+export default store
